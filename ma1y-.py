240b400806be24d2ec7a340bb6477695a1c5c208
codeup26 <86265604+codeup26@users.noreply.github.com>
2022-01-16 15:24:55 -0500

    Add files via upload

---
+++ ma1y-.py
@@ -0,0 +1,23 @@
+import time
+import random
+
+
+#Get start with Python
+print('booting [_________]')
+time.sleep(random.randint(1, 10))
+print('booting [#######__]loading')
+time.sleep(random.randint(1, 10))
+print('booting [########_]loading')
+time.sleep(random.randint(1, 10))
+print('booting [#########]loading')
+while (0 == 0):
+    while (0 == 0):
+        if (input('">>>') == 'say'):
+            print(input('print:'))
+            continue
+        while (0 == 0):
+            if (input('">>>') == 'help'):
+                print('found data is//')
+                print('help')
+                print('say')
+                break
